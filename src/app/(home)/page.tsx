26f1c30cf6b28931d8f8bbe17450ce99c75cb0fd
Ratchanon Pheungta <ratchanon@topgunthailand.com>
2024-04-26 15:04:09 +0700

    feat: implement home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,32 @@
+"use client";
+import Image from "next/image";
+import undawnSvg from "@images/undraw_programming.svg";
+import { TypeAnimation } from "react-type-animation";
 export default function Page() {
   return (
-    <div className="hero">
-      <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Hello there</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+    <div className="w-full h-full flex  flex-col justify-center">
+      <div className="flex flex-row flex-wrap-reverse items-center justify-center">
+        <div className="sm:min-w-[450px]">
+          <TypeAnimation
+            sequence={["Hello There!", 1000]}
+            speed={30}
+            repeat={Infinity}
+            className="text-5xl font-bold"
+          />
+
+          <p className="md:py-6 py-3 text-2xl md:text-4xl">
+            {"I'm"} <span className="text-primary">{"Ratchanon Pheungta"}</span>
           </p>
-          <button className="btn btn-primary">Get Started</button>
+
+          <p className="text-2xl md:text-4xl">Web and Mobile Developer</p>
+          <button className="btn btn-primary mt-6">Get Started</button>
         </div>
+        <Image
+          priority
+          src={undawnSvg}
+          alt="Programmer"
+          className="w-full max-w-[450px] object-contain my-10"
+        />
       </div>
     </div>
   );
