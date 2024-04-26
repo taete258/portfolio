b758d3829260f843362fa21a15c890c24abacc2c
Ratchanon Pheungta <ratchanon@topgunthailand.com>
2024-04-26 15:03:49 +0700

    chore: improve navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import ThemeSwitch from "./theme-switch";
 import useThemeStore from "@/stores/useThemeStore";
 import Link from "next/link";
+import { FaBurger } from "react-icons/fa6";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function Navbar() {
   const theme = useThemeStore((state) => state.theme);
@@ -10,16 +12,44 @@ export default function Navbar() {
   return (
     <div className="navbar bg-base-100 sticky top-0" data-theme={theme}>
       <div className="flex-1">
-        <Link className="btn btn-ghost text-xl" href={"/"}>
-          Portfolio
+        <Link className="btn btn-ghost text-xl hidden sm:flex" href={"/"}>
+          Ratchanon Pheungta
         </Link>
+        <div className="drawer p-0 sm:hidden">
+          <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content ">
+            {/* Page content here */}
+            <label htmlFor="my-drawer" className=" drawer-button ">
+              <GiHamburgerMenu size={28} className="ml-4" />
+            </label>
+          </div>
+          <div className="drawer-side">
+            <label
+              htmlFor="my-drawer"
+              aria-label="close sidebar"
+              className="drawer-overlay"
+            ></label>
+            <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content m-0">
+              {/* Sidebar content here */}
+              <li>
+                <a>Sidebar Item 1</a>
+              </li>
+              <li>
+                <a>Sidebar Item 2</a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
-          <li>
+          <li className="hidden sm:flex">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="hidden sm:flex">
             <Link href="/about-me">About Me</Link>
           </li>
-          <li>
+          <li className="hidden sm:flex">
             <Link href="/contacts">Contact</Link>
           </li>
           <li>
