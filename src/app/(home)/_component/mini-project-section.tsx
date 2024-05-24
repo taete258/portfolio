d102b8afa9ccad395d48a53315759c773e216fc4
Ratchanon Pheungta <ratchanon@topgunthailand.com>
2024-05-24 15:06:36 +0700

    feat: add data in mini project section

--- src/app/(home)/_component/mini-project-section.tsx
+++ src/app/(home)/_component/mini-project-section.tsx
@@ -1,67 +1,89 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import React from "react";
+import WhettImage from "@images/whett-web-screen.png";
+import CatExImage from "@images/cat-explorer-screen.png";
+import Link from "next/link";
+import { SiGithub } from "react-icons/si";
 
+type ProjectType = {
+  title: string;
+  description: string;
+  github: string;
+  link?: string;
+  previewImage: {
+    source: StaticImageData;
+    alt: string;
+  };
+};
+const projectDataList: ProjectType[] = [
+  {
+    title: "Whett",
+    description:
+      "A web application create with next.js that allows you to search and show some data about weather.",
+    github: "https://github.com/taete258/nextjs-whett-project",
+    link: "https://nextjs-test-psi-mocha.vercel.app",
+    previewImage: {
+      source: WhettImage,
+      alt: "Whett",
+    },
+  },
+  {
+    title: "Cat Explorer",
+    description:
+      "A mobile application create with react native that to show details about cats.",
+    github: "https://github.com/taete258/cat-explorer",
+    previewImage: {
+      source: CatExImage,
+      alt: "Cat Explorer",
+    },
+  },
+];
 const MiniProjectSection: React.FC = () => {
   return (
-    <div className="space-y-8 max-w-7xl mx-auto my-10">
+    <div className="space-y-8 max-w-5xl mx-auto my-10">
       <h1 className="text-5xl font-bold">My Mini Projects</h1>
       <div className="flex flex-row flex-wrap items-center justify-around w-full gap-6">
-        <div className="card bg-base-100 shadow-lg carousel-item max-w-[270px] sm:mr-1">
-          <figure>
-            <Image
-              src="https://picsum.photos/seed/picsum/400/300"
-              alt="Shoes"
-              width={200}
-              height={100}
-              className="object-cover object-center  w-full"
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary text-white">Buy Now</button>
-            </div>
-          </div>
-        </div>
+        {projectDataList.map((item, index) => (
+          <div key={index} className="card bg-base-100 shadow-lg">
+            <div className="card-body flex-row flex-wrap items-center justify-center gap-6  p-5  sm:p-8">
+              <div className="flex flex-row gap-x-2 max-w-[380px]">
+                <Image
+                  src={item.previewImage.source}
+                  alt={item.previewImage.alt}
+                  quality={100}
+                  className={`object-contain h-[220px] shadow-md bg-gray-200 rounded-md`}
+                />
+              </div>
 
-        <div className="card bg-base-100 shadow-lg carousel-item max-w-[270px] sm:mr-1">
-          <figure>
-            <Image
-              src="https://picsum.photos/seed/picsum/400/300"
-              alt="Shoes"
-              width={200}
-              height={100}
-              className="object-cover object-center  w-full"
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary text-white">Buy Now</button>
+              <div className="flex flex-1 flex-col sm:min-w-96 gap-y-3">
+                <h1 className="font-bold text-2xl text-primary">
+                  {item.title}
+                </h1>
+                <p className="text-lg">{item.description}</p>
+                <div className="card-actions justify-end">
+                  {item.github && (
+                    <Link
+                      className="btn btn-primary text-white"
+                      href={item.github}
+                      target="_blank"
+                    >
+                      <SiGithub size={28} />
+                    </Link>
+                  )}
+                  {item.link && (
+                    <Link
+                      className="btn btn-primary text-white"
+                      href={item.link}
+                      target="_blank"
+                    >
+                      Go to Site
+                    </Link>
+                  )}
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className="card bg-base-100 shadow-lg carousel-item max-w-[270px] sm:mr-1">
-          <figure>
-            <Image
-              src="https://picsum.photos/seed/picsum/400/300"
-              alt="Shoes"
-              width={200}
-              height={100}
-              className="object-cover object-center  w-full"
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary text-white">Buy Now</button>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
