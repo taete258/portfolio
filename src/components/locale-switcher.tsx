7933dcafc9547976db357933a26c31554bdc5782
ratchanon.p <ratchanon.p@chanwanich.digital>
2025-09-18 16:53:05 +0700

    feat: enhance LocaleSwitcher component with animation and styling improvements
    - Integrated framer-motion for animated highlight effect in the LocaleSwitcher.
    - Updated button styles to include a border and background color changes for better visibility and user interaction.
    - Improved the highlight element to use motion for a smoother visual transition.

--- src/components/locale-switcher.tsx
+++ src/components/locale-switcher.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { motion } from "framer-motion";
 import { useLocale } from "next-intl";
 import { usePathname, useRouter } from "@/i18n/routing";
 import { useTransition } from "react";
@@ -24,7 +25,7 @@ export default function LocaleSwitcher() {
   };
 
   return (
-    <div className="absolute bottom-8 right-8 flex items-center space-x-2 rounded-full bg-gray-900/50 p-1">
+    <div className="absolute bottom-8 right-8 flex items-center space-x-2 rounded-full bg-gray-900/50 p-1 border hover:border-primary bg-secondary">
       {languages.map((lang) => (
         <button
           key={lang.code}
@@ -38,9 +39,9 @@ export default function LocaleSwitcher() {
           )}
         >
           {locale === lang.code && (
-            <div
-              id="locale-switcher-highlight"
-              className="absolute inset-0 bg-sky-500 rounded-full"
+            <motion.div
+              layoutId="locale-switcher-highlight"
+              className="absolute inset-0 bg-primary rounded-full"
               style={{ borderRadius: 9999 }}
             />
           )}
