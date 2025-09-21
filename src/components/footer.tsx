5ecf3fb14d41a4f5450beb0d5a79db6ee1672044
ratchanon.p <ratchanon.p@chanwanich.digital>
2025-09-21 19:46:44 +0700

    feat: add id attribute to Footer component for improved accessibility
    - Added an id attribute to the Footer component to enhance accessibility and allow for easier navigation to the contact section.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -36,6 +36,7 @@ const Footer = () => {
 
   return (
     <motion.footer
+      id="contact"
       initial={{ opacity: 0, y: 20 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8, ease: "easeOut" }}
